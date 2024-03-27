cc12ce241d76a4c9790547ed5eff10d15238eada
V-Markovskis <vlads.markovskis@gmail.com>
2024-03-27 21:10:59 +0200

    DataTables added

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,54 +1,79 @@
-<template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Hello from Vue component</div>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
 
-                    <div class="card-body">
-                        <div v-if="isLoading">
-                            Loading...
-                        </div>
-                        <div v-else>
-                            <div v-for="(article, index) in articles" :key="index">
-                                <h2>{{ article.title }}</h2>
-                                <h3>{{ article.link }}</h3>
-                                <h3>{{ article.points }}</h3>
-                                <h3>{{ article.date_created}}</h3>
-                                <span>======================</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+import DataTable from 'datatables.net-vue3';
+import DataTablesLib from 'datatables.net';
+import 'datatables.net-select';
+import axios from "axios";
+
+DataTable.use(DataTablesLib);
+
+let dt;
+const data = ref([]);
+const table = ref();
+const columns = [
+    {
+        data: 'title',
+        title: 'Title',
+    },
+    {
+        data: 'link',
+        title: 'Link',
+    },
+    {
+        data: 'points',
+        title: 'Points',
+    },
+];
+
+
+// Get a DataTables API reference
+onMounted(async () => {
+    dt = table.value.dt;
+    await fetchData();
+});
+
+async function fetchData() {
+    try {
+        const response = await axios.get('/api/scrape-and-save');
+        data.value = response.data.data;
+        console.log('Data fetched:', data.value);
+    } catch (error) {
+        console.error('Failed to fetch data: ', error);
+    }
+}
+
+
+// For each selected row find the data object in `data` array and remove it
+function remove() {
+    dt.rows({ selected: true }).every(function () {
+        let idx = data.value.indexOf(this.data());
+        data.value.splice(idx, 1);
+    });
+}
+</script>
+
+<template>
+    <div>
+        <h1>Simple table</h1>
+        <h2>DataTables + Vue3 example</h2>
+        <p>
+            This example demonstrates the <code>datatables.net-vue3</code> package
+            being used to display an interactive DataTable in a Vue application.
+        </p>
+
+        <button @click="remove">Delete selected rows</button>
+
+        <DataTable
+            class="display"
+            :columns="columns"
+            :data="data"
+            :options="{ select: true }"
+            ref="table"
+        />
     </div>
 </template>
 
-<script lang="ts">
-    import axios from "axios";
+<style>
 
-    export default {
-        data() {
-            return {
-                articles: [] as { title: string, link: string, points: string, date_created: string }[],
-                isLoading: true
-            };
-        },
-        mounted() {
-            this.fetchData();
-        },
-        methods: {
-            async fetchData() {
-                try {
-                    const response = await axios.get('/api/scrape-and-save');
-                    this.articles = response.data.data;
-                } catch (error) {
-                    console.error('Failed to fetch data: ', error);
-                } finally {
-                    this.isLoading = false;
-                }
-            }
-        }
-    }
-</script>
+</style>
