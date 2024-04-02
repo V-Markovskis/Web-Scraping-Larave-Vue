d00d52a614b09613f6030d361a634ac0e17eb53a
V-Markovskis <vlads.markovskis@gmail.com>
2024-04-02 19:21:20 +0300

    auth test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -31,7 +31,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/scraper';
+    protected $redirectTo = '/home';
 
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/scraper', function () {
-    return view('welcome');
-});
+//Route::get('/scraper', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/', function () {
+//    return view('home');
+//});
 
 Route::get('/scrape-and-save', [ScrapeController::class, 'scrapeAndSave']);
 
